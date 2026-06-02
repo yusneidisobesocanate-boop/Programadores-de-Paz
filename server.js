650ebfe5b0ace4d16083d4c61677bc60c2f32386
yusneidisobesocanate-boop <yusneidisobesocanate@gmail.com>
2026-06-01 19:28:52 -0500

    SEMANA 5
    SEMANA 5

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente');
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
 });
 
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
+});
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
 });
